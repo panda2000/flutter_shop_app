c57a7d36f6dc81cba07f72f1587793586d012c18
pandaprg <pandaprg@gmail.com>
2020-01-23 22:04:30 +0400

    Using a Side Drawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../providers/orders.dart' show Orders;
 
 import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
 
 class OrdersScreen extends StatelessWidget {
 
@@ -16,6 +17,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar (
         title: Text('Your orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: ordersData.orders.length,
         itemBuilder: (ctx,i) => OrderItem(ordersData.orders[i]),
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -3,7 +3,10 @@ import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
+import '../widgets/app_drawer.dart';
+
 import '../providers/cart.dart';
+
 import './cart_screen.dart';
 
 enum FilterOptions {
@@ -65,6 +68,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
