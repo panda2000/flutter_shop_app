ee3bf7d1e32fb8a616b7226f3e9b7009cdd0de8c
pandaprg <pandaprg@gmail.com>
2020-01-25 11:23:47 +0400

    Snackbar & Undoing dialog

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -54,6 +54,19 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("Add item into chart!"),
+                    duration: Duration(seconds: 2),
+                    action: SnackBarAction(
+                      label: "UNDO",
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      },
+                    ),
+                  )
+              );
             },
             color: Theme.of(context).accentColor,
           ),
