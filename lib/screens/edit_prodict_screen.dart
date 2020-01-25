0a4d1c444a3f28bb68c552e9a1de03f3e155f781
pandaprg <pandaprg@gmail.com>
2020-01-25 22:51:26 +0400

    Image input

--- lib/screens/edit_prodict_screen.dart
+++ lib/screens/edit_prodict_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbols.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const String routeName = "/edit-product";
@@ -11,12 +12,31 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl () {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {
+        
+      });
+    }
+  }
 
   @override
   void dispose() {
     // TODO: implement dispose
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.dispose();
     super.dispose();
   }
 
@@ -49,10 +69,40 @@ class _EditProductScreenState extends State<EditProductScreen> {
               ),
               TextFormField (
                 decoration: InputDecoration (labelText: "Description",),
-                maxLength: 3,
+                maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocusNode,
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(top:8, right: 10),
+                  decoration: BoxDecoration (
+                    border: Border.all (
+                      width: 1,
+                      color: Colors.grey,
+                    )
+                  ),
+                  child: _imageUrlController.text.isEmpty ? Text ("Enter a URL") : FittedBox (
+                    child: Image.network(
+                      _imageUrlController.text,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration (labelText: 'Image URL'),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlController,
+                    focusNode: _imageUrlFocusNode,
+                  ),
+                )
+              ],)
             ],
           ),
         ),
