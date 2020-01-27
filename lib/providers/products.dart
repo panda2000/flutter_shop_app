32b88af64d71a32a40a58c469db324595911e5dd
pandaprg <pandaprg@gmail.com>
2020-01-27 21:27:27 +0400

    Sending POST request

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -65,6 +68,16 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+
+    const url = 'https://shop-app-fdbbb.firebaseio.com/';
+    http.post(url, body: json.encode({
+      'title':product.title,
+      'description': product.description,
+      'price':product.price,
+      'imageUrl':product.imageUrl,
+      'isFavorite':product.isFavorite,
+    }),);
+
     final newProduct = Product (
       title: product.title,
       description: product.description,
