76bf4338bf5a9b159334bda78ba7d793ec4fa01a
pandaprg <pandaprg@gmail.com>
2020-02-01 21:16:37 +0400

    Updating Favorite status optimistic strategy

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +21,28 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void toggleFavoriteStatus() async {
+    var oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = 'https://shop-app-fdbbb.firebaseio.com/products/$id.json';
+    try {
+      final respone = await http.patch(
+          url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          })
+      );
+
+      if (respone.statusCode >= 400) {
+        throw HttpException('Update Favorite fail');
+      } else {
+        oldStatus = null;
+      }
+    } catch (error) {
+      isFavorite = oldStatus;
+      notifyListeners();
+    }
   }
 }
