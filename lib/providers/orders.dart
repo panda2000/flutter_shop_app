07100a8821bec4106d24fa02d8ef5053b7f6aee5
pandaprg <pandaprg@gmail.com>
2020-02-01 22:00:15 +0400

    Storing orders

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 import './cart.dart';
 
@@ -24,14 +27,28 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future <void> addOrder(List<CartItem> cartProducts, double total) async {
+
+    const url = 'https://shop-app-fdbbb.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    final responce = await http.post(url, body: json.encode({
+      'amount':total,
+      'dateTime':timestamp.toIso8601String(),
+      'products': cartProducts.map((cp) => {
+          'id': cp.id,
+          'title': cp.title,
+          'quantity': cp.quantity,
+          'price' : cp.price,
+        }).toList(),
+
+    }));
     _orders.insert(
         0,
         OrderItem(
-            id: DateTime.now().toString(),
+            id: json.decode(responce.body)['name'],
             amount: total,
             products: cartProducts,
-            dateTime: DateTime.now()
+            dateTime: timestamp,
         )
     );
     notifyListeners();
