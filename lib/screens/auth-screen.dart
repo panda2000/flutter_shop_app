5b0b2f99480766e62da2c689a6970598258b3a8a
pandaprg <pandaprg@gmail.com>
2020-02-09 22:36:05 +0400

    Animated Contaner

--- lib/screens/auth-screen.dart
+++ lib/screens/auth-screen.dart
@@ -112,7 +112,7 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
   void initState() {
     _controller = AnimationController (
         vsync: this,
-        duration: Duration(microseconds: 300),
+        duration: Duration(microseconds: 800),
     );
     _hightAnimation = Tween<Size> (
         begin: Size(double.infinity, 260),
@@ -217,14 +217,15 @@ class _AuthCardState extends State<AuthCard> with SingleTickerProviderStateMixin
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder (animation: _hightAnimation, builder: (ctx, ch) => Container(
-        //height: _authMode == AuthMode.Signup ? 320 : 260
-        height: _hightAnimation.value.height,
-        constraints:
-        BoxConstraints(minHeight: _hightAnimation.value.height),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        //height: _hightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260,),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
-        child: ch), child: Form(
+        child: Form(
         key: _formKey,
         child: SingleChildScrollView(
           child: Column(
